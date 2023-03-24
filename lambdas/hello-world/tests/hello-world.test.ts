29002406259684f1c6e4e98f7801874e4cf41364
Dougal Rees <24165331+DougsCER@users.noreply.github.com>
2023-03-24 11:55:27 +0000

    GUA-720 hello world lambda

---
+++ lambdas/hello-world/tests/hello-world.test.ts
@@ -0,0 +1,23 @@
+import "aws-sdk-client-mock-jest";
+import { mockClient } from "aws-sdk-client-mock";
+import { helloWorld } from "../hello-world";
+
+
+describe("hello-world", () => {
+    let consoleMock: jest.SpyInstance;
+
+    beforeEach(() => {
+        consoleMock = jest 
+        .spyOn(global.console, "info")
+        .mockImplementation();
+    });
+
+    afterEach(() => {
+        consoleMock.mockRestore();
+    })
+
+    test("hello-world-prints-to-console", () => {
+     helloWorld();
+     expect(consoleMock).toHaveBeenCalledWith("Hello world");
+    });
+}); 
