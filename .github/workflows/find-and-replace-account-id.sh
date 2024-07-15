75585965d3ea844d01c31c09ab2f44ac3e75ac70
Toby Saunders <110459361+TobySaundersGDS@users.noreply.github.com>
2024-07-15 11:41:48 +0100

    Update find-and-replace-account-id.sh
    correct escape chars

--- .github/workflows/find-and-replace-account-id.sh
+++ .github/workflows/find-and-replace-account-id.sh
@@ -1,7 +1,7 @@
 #!
 if grep -q "<fill-in-account-id>" $GITHUB_WORKSPACE/infrastructure/template.yaml; then
     echo "Replacing \"<fill-in-account-id>" with new ECR image ref"
-    sed -i "s|/<fill-in-account-id>/|$TEST_AWS_ACCOUNT|" $GITHUB_WORKSPACE/infrastructure/template.yaml
+    sed -i "s|\<fill-in-account-id\>|$TEST_AWS_ACCOUNT|" $GITHUB_WORKSPACE/infrastructure/template.yaml
 else
     echo "WARNING!!! Image placeholder text \"\<fill-in-account-id\>" not found - uploading template anyway"
 fi
